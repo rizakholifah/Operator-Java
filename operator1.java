9621dcd58cf7c8711e80ba50a9bff9ad43dbad7e
Riza Kholifah Akwan <riza.kholifah05@gmail.com>
2019-12-09 10:52:08 +0700

    Menerakan operator aritmatika pada file operator1.java

---
+++ operator1.java
@@ -0,0 +1,12 @@
+public class operator1{
+    public static void main(String[] args){
+        int a = 7;
+        int b = 5;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b);
+        System.out.println(a*b);
+
+    }
+}
