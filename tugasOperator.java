3f22bb511db6179e1c2f7a7ebd91319601a722b0
Riza Kholifah Akwan <riza.kholifah05@gmail.com>
2019-12-09 13:29:28 +0700

    Membuat dan mengedit file tugasOperator.java

---
+++ tugasOperator.java
@@ -0,0 +1,10 @@
+public class tugasOperator{
+    public static void main(String[] args){
+        int nilai = Integer.parseInt(args[0]);
+        String cek = nilai%2 == 0 ? "Genap":"Ganjil";
+
+        System.out.println(cek);
+        
+    }
+
+}
