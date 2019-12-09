ad18f5b88e373d1c4f54be9255a6510152dfa98e
Riza Kholifah Akwan <riza.kholifah05@gmail.com>
2019-12-09 12:08:39 +0700

    Edit insturksi operator  perbandingan di file operator2.java

---
+++ operator2.java
@@ -0,0 +1,18 @@
+public class operator2{
+    public static void main(String[] args){
+        int a = 7;
+        int b = 5;
+        String x = "Merah";
+        String y = "Biru";
+        
+        System.out.println("x > b --> "+(a>b));
+        System.out.println("x < b --> "+(a<b));
+        System.out.println("x = b --> "+(a>=b));
+        System.out.println("x >= b --> "+(a>=b));
+        System.out.println("x == b --> "+(a==b));
+        System.out.println("x > b --> "+(a!=b));
+        System.out.println("Merah == Biru --> "+(a == b));
+        System.out.println("Merah != Biru --> "+(a != b));
+
+    }
+}
