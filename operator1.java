74fa293480743298ff3a5195be8ea17fadf20e2e
Riza Kholifah Akwan <riza.kholifah05@gmail.com>
2019-12-09 11:23:21 +0700

    Edit increment dan decrement

--- operator1.java
+++ operator1.java
@@ -12,6 +12,10 @@ public class operator1{
         System.out.println(a);
         b++;
         System.out.println(b);
+        a--;
+        System.out.println(a);
+        b--;
+        System.out.println(b);
 
     }
 }
