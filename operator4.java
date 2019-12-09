0ca5167bc8e3a4d0df554eb3bc5aa4cd41bdfe4c
Riza Kholifah Akwan <riza.kholifah05@gmail.com>
2019-12-09 12:46:21 +0700

    Menambahkan dann edit file operator4.java

---
+++ operator4.java
@@ -0,0 +1,14 @@
+public class operator4{
+    public static void main(String[] args){
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println(a && b);
+        System.out.println(a || b);
+        System.out.println(!a);
+
+        System.out.println(!a && b);
+        System.out.println(!(a && b));
+        
+    }
+}
